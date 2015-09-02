0bca625153d266b50d49d2d31f6217aa28f30a0b
ArvenAnna <LastSunsetBeam@yandex.ru>
2015-09-02 22:51:04 +0300

    Update gallery.php

--- Flower_site/gallery.php
+++ Flower_site/gallery.php
@@ -1,5 +1,6 @@
 <?php
 include "admin/functions.php";
+include "navigation.php";
 ?>
 
 <html>
@@ -58,56 +59,9 @@ include "admin/functions.php";
 						$j++;
 					}
 					
-				$pagescount=$pages+1;
-		
-				$gal=new Galery($items);
-		
-		
-				if ($_REQUEST['help'])
-					{
-						$gal->set_previous_page($_REQUEST['help']);
-					}
-				$i=0;
 				
-				while($i<$pagescount)
-					{
-						if($_REQUEST["flower$i"])
-							{
-								$gal->set_current_page($i,$items);
-							}
-						$i++;
-					}
-				if ($_REQUEST['back'])
-					{
-						$gal->back($items);
-					}
-				if ($_REQUEST['forward'])
-					{
-						$gal->forward($items);
-					}
-
-				echo $gal->content;
-				echo "<form action=$form_action name='navigation'><input type='hidden' name='help' value=$gal->current_page></input>";
+				navigation($form_action,$items,$pages);
 		
-				if($gal->current_page!=0)
-					{
-						echo "<input type='submit' name='back' value='Назад'></input>";
-					}
-
-				$i=0;
-				while ($i<$pagescount)
-					{
-						echo "<input type='submit' name='flower".$i."' value='".($i+1)."'></input>";
-						$i++;
-					}
-					
-				if($gal->current_page!=$pages)
-					{
-						echo "<input type='submit' name='forward' value='Вперед'></input>";
-					}
-				echo "</form>";
-				echo "Это вставить где надо - текущая страница ".($gal->current_page+1);
-
 			?>
 		</div>
 	</div>
