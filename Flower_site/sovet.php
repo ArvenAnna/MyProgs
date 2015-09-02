ff1b74bbc35326a47cccb19da30ed74c47da3f11
ArvenAnna <LastSunsetBeam@yandex.ru>
2015-09-02 22:52:15 +0300

    Create sovet.php

---
+++ Flower_site/sovet.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<title>house rose
+</title>
+</head>
+<body>
+<link href="css/style.css" rel="stylesheet">
+<link href="css/framestyle.css" rel="stylesheet">
+
+
+<div class="header site">
+<?php
+include "parts/header.html";
+?>
+</div>
+
+
+
+<div class="menu site">
+<?php
+include "parts/menu.html";
+?>
+</div>
+
+
+<div class="main-content site">
+<div class="text-content">Условия содержания</div>
+</div>
+
+
+<div class="footer site">
+<?php
+include "parts/footer.html";
+?>
+</div>
+
+
+
+</body>
+
+</html>
